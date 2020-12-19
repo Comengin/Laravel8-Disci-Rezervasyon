c8c5f54db32001a3ba4e764fc43bf0f66cc51b06
Comengin <kuliev.bairam@yandex.ru>
2020-12-20 00:52:40 +0300

    index changing

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>First Laravel Page</title>
-</head>
-<body>
+@extends('layouts.home')
 
-<h1>Welcome to Laravel</h1>
-<p>This is a paragraph.</p>
-<br>
-<a href="{{route('test', ['id'=>12, 'name'=>'Fatma'])}}">Test sayfa</a>
-</body>
-</html>
 
