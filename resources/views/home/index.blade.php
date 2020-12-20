623daafd81da158c13a18754dcf9a028ffb54b10
Comengin <kuliev.bairam@yandex.ru>
2020-12-20 03:47:09 +0300

    index changing

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,3 +1,14 @@
 @extends('layouts.home')
 
+@section('title', 'Page Title')
+
+@section('sidebar')
+    @parent
+
+    <p>This is appended to the master sidebar.</p>
+@endsection
+
+@section('content')
+    <p>This is my body content.</p>
+@endsection
 
