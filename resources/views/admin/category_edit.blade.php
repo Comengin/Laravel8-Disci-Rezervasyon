8043bebda088ea5d14acb430ef695637c3aedaab
Comengin <kuliev.bairam@yandex.ru>
2021-01-05 01:44:06 +0300

    edit category: category.blade.php category_edit.blade.php CategoryController.php

---
+++ resources/views/admin/category_edit.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.admin')
+
+@section('title', 'Edit Category')
+
+@section('content')
+    <div class="dashboard-wrapper">
+        <div class="container-fluid dashboard-content">
+            <div class="row">
+                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                    <h3 class="text-center">Editting Category</h3>
+                    <div class="row">
+                        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                            <div class="section-block" id="basicform">Edit category</div>
+                            <div class="card">
+                                <div class="card-body">
+                                    <form action="{{route('admin_category_update',['id'=>$data->id])}}" method="post">
+                                        @csrf
+                                        <div class="form-group">
+                                            <label for="inputText3" class="col-form-label">Parent</label>
+                                            <select class="form-control" name="parent_id">
+                                                <option value="0">Main category</option>
+                                                @foreach($datalist as $rs)
+                                                    <option value="{{$rs->id}}" @if($rs->id == $data->parent_id) selected="selected" @endif>{{$rs->title}}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="inputText3" class="col-form-label">Title</label>
+                                            <input id="inputText3" type="text" class="form-control" value="{{$data->title}}" name="title">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="inputText3" class="col-form-label">Keywords</label>
+                                            <input id="inputText3" type="text" class="form-control" value="{{$data->keywords}}" name="keywords">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="inputText3" class="col-form-label">Description</label>
+                                            <input id="inputText3" type="text" class="form-control" value="{{$data->description}}" name="description">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Status</label>
+                                            <select class="form-control" name="status">
+                                                <option selected="selected">{{$data->status}}</option>
+                                                <option>False</option>
+                                                <option>True</option>
+                                            </select>
+                                        </div>
+                                        <div>
+                                            <button class="btn btn-primary" type="submit">Update category</button>
+                                        </div>
+                                    </form>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+@endsection
